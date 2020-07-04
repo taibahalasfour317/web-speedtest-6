a733052f84f0910fce795743e3b52205c8bb2e0b
طيبة جمال العصفور -317 <taibahalasfour224@gmail.com>
2020-07-04 17:13:51 +0300

    done!!

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -7,11 +7,23 @@
  * - name إن استقبلت اسم أو "Hello <name>" تطبع
  */
 
-function greet(name) {}
+function greet(name) {
+if(name!==done)
+console.log("hello")
+else(console.log("hello" + name))
+
+}
 
 /*
  * isOdd(n) اسم الدالة
  * - n تستقبل رقم
  * - عدا ذلك false إن كان الرقم فردياً وترجع true ترجع القيمة
  */
-function isOdd(n) {}
+function isOdd(n) {
+if( n %2 ==0){
+return true ;
+}
+else{
+return false;
+
+}
